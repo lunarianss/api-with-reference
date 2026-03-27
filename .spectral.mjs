c24db3825845ae54f717581562fcb083282b6db5
SEvans <0uh3tn5n@bccto.cc>
2026-03-27 18:07:34 +0800

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMzY3MQ/spectral.js?branch_slug=main&token=6ede6542-1c62-45bc-ac31-4a7c5f65ad14";
+export default { extends: ruleset };
